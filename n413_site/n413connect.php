3ba8504eddf8c27e7f5ef5501275b23f59800bf2
kalnking <54544529+kalnking@users.noreply.github.com>
2020-09-16 20:02:46 -0400

    Add files via upload

---
+++ n413_site/n413connect.php
@@ -0,0 +1,9 @@
+<?php
+                $dbhost = 'localhost:8889'; //XAMPP is 'localhost:3306'
+                $dbuser = 'root';
+                $dbpwd  = 'root'; //XAMPP password is ''
+                $dbname = 'list';
+
+                $link = mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
+                if (!$link) { die('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error()); }
+?>
